c88d914daef115f98fa5d6442aa5f86149b7f6f6
vipul negi <167672450+VIPUlNEGI1@users.noreply.github.com>
2024-09-08 17:47:42 +0530

    Add files via upload

---
+++ Apicontext-mainfile/contextapi2/Them.js
@@ -0,0 +1,12 @@
+import { createContext,useContext } from "react";
+export const ThemeContext = createContext({
+    themeMode : 'light',
+    darkThem:()=>{},
+   lightThem:()=>{},
+
+})
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+return useContext(ThemeContext)
+}
